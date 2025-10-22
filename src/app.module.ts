7a63b687b04ba7c4455a4838fc8394d74a2d7388
jokkolr <169060103+jokkolr@users.noreply.github.com>
2025-10-22 09:51:19 +0300

    Uncomment AuthModule import in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';  // 👈 Import the Auth module
+import { AuthModule } from './auth/auth.module';  // 
 
 @Module({
   imports: [
-    UsersModule,  // 👈 Users module we made earlier
-    AuthModule,   // 👈 Auth module we just created
+    UsersModule,  // 
+    AuthModule,   //
   ],
 })
 export class AppModule {}
