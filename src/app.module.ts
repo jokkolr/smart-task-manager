2b89d9b8e5c94253f208dbe1f0ee87aae01e2f0b
jokkolr <169060103+jokkolr@users.noreply.github.com>
2025-10-22 11:09:27 +0300

    Delete src directory

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { TasksModule } from './tasks/tasks.module';
-
-@Module({
-  imports: [UsersModule, AuthModule, TasksModule],
-})
-export class AppModule {}
